59d7330d401533f4e6aa01bb14ebbeb6938a7c6e
Bhavishyanirmal <nirmalbhavishya25@gmail.com>
2024-12-18 15:55:36 +0530

    Update

---
+++ fibbonaci sequence.py
@@ -0,0 +1,18 @@
+
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n=int(input("enter number:"))
+if n<0:
+    print("enter valid number")
+else:
+        
+    for i in range(n):
+        print(fibonacci(i))
+    
+    
+    
